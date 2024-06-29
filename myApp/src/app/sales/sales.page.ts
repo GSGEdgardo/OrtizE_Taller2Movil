8be31c4462ca34fa59442504d31cbcacc4d6feeb
Edgardo Ortiz <edgardo.ortiz@alumnos.ucn.cl>
2024-06-29 05:22:52 -0400

    feat: add logout functionality

--- myApp/src/app/sales/sales.page.ts
+++ myApp/src/app/sales/sales.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../services/account.service';
 import { SalesService } from '../services/sales.service';
 import { Sale } from '../models/sale';
-import { AccountService } from '../services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sales',
@@ -11,7 +12,7 @@ import { AccountService } from '../services/account.service';
 export class SalesPage implements OnInit {
   sales: Sale[] = [];
 
-  constructor(private salesService: SalesService, private accountService: AccountService) {}
+  constructor(private salesService: SalesService, private accountService: AccountService, private router: Router) {}
 
   ngOnInit() {
     const currentUser = this.accountService.currentAccountValue;
@@ -20,5 +21,10 @@ export class SalesPage implements OnInit {
         this.sales = sales;
       });
     }
+  }
+
+  logout(): void {
+    this.accountService.logout();
+    this.router.navigateByUrl('/login');
   }
 }
